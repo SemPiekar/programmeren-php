2ae4283169fd4dd7d66280602fc7d932bdd53277
SemPiekar <9018659@student.zadkine.nl>
2022-12-12 12:38:26 +0100

    opdracht 3.2

---
+++ opdracht_3.2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>opdracht 3.2</title>
+</head>
+<body>
+    <?php
+
+    $name = "Sem Piekar";
+    $age1 = 6;
+    $age2 = 10;
+    $age3 = $age1 + $age2;
+    echo "<p>Ik ben $name en ik ben $age3 jaar oud</p>"
+    ?>
+
+</body>
+</html>
