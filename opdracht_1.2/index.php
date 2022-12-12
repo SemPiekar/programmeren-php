0dc828a05a838d1d4107df63a7b6715fddab0ff6
SemPiekar <9018659@student.zadkine.nl>
2022-12-12 12:31:46 +0100

    Opdracht 2

--- opdracht_1.2/index.php
+++ opdracht_1.2/index.php
@@ -5,37 +5,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>opdracht 2</title>
 </head>
 
 <body>
     <?php
-    date_default_timezone_set("Europe/Amsterdam");
-
-    $today = date("l d F Y ");
-    echo "<p>Het is vandaag: $today</p>";
-
-    $yearday = date("z") . "e";
-    echo "<p>Vandaag is het de $yearday dag van het jaar</p>";
-
-    $day = date("l");
-    $daynumber = date("w") . "e";
-    echo "<p>$day is de $daynumber dag van de week</p>";
-
-    $month = date("F");
-    $totaldays = date("j");
-    echo "<p>de maand $month heeft in totaal $totaldays dagen</p>";
-
-    $year = date("Y");
-    $leapyearcheck = date("L");
-
-
-    if ($leapyearcheck > 0) {
-        print("Het jaar $year is een schrikkel jaar");
-    } else {
-        print("Het jaar $year is geen schrikkel jaar");
-    }
+    // Hierdoor komt "Hello World" op het scherm
+    /*
+    Parse error: syntax error, unexpected end of file in C:\xampp\htdocs\programmeren-php\index.php on line 12
+    Dit betekent dat er geen ; op het einde is van line 12, oftwel unexpected end of file    
+    */
+    echo "<p>Hello World!</p>"
+    # PHP close tag
     ?>
+    
 </body>
 
 </html>
