16799dbd7b7b354b182c8d0ba0a0b7ee12bd42d3
SemPiekar <9018659@student.zadkine.nl>
2023-03-21 11:24:26 +0100

    opdracht 6_4

---
+++ opdracht_6_4/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>opdracht 4</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="radius">Straal:</label>
+        <input type="number" name="radius" required>
+        <input type="submit" name="submit" value="berekenen">
+    </form>
+
+<?php
+
+if(isset($_POST['submit'])) {
+    $r = $_POST['radius'];
+
+    echo "De omtrek van de cirkel met straal " . $r . ' is: ' . calc_circum($r) . "<br>";
+    echo "De opperrvlakte van de cirkel is: " . calc_area($r);  
+}
+
+function calc_circum($radius){
+$circum = round(2 * pi() * $radius, 2);
+return $circum;
+}
+
+function calc_area($radius){
+$area = round(pi() * $radius * $radius,2);
+return $area;
+}
+
+?>
+
+</body>
+</html>
